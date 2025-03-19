ba879198f02acceaa2722d656d94465c250020e0
gesttaltt <jonathan.verdun707@gmail.com>
2025-03-19 20:14:46 -0300

    Fix: typescript prop errors and UI components extra imports

--- frontend/src/components/ui/Input.tsx
+++ frontend/src/components/ui/Input.tsx
@@ -1,3 +1,6 @@
+/*
+ Input.tsx
+*/
 import React, { InputHTMLAttributes, forwardRef } from "react";
 
 export interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
@@ -5,11 +8,11 @@ export interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
 }
 
 const Input = forwardRef<HTMLInputElement, InputProps>(
-  ({ label, name, className, ...rest }, ref) => {
+  ({ label, name, className = "", ...rest }, ref) => {
     return (
-      <div className="flex flex-col">
+      <div className="flex flex-col my-4">
         {label && name && (
-          <label htmlFor={name} className="m-4 text-white">
+          <label htmlFor={name} className="mb-2 text-white">
             {label}
           </label>
         )}
@@ -17,7 +20,7 @@ const Input = forwardRef<HTMLInputElement, InputProps>(
           ref={ref}
           id={name}
           name={name}
-          className={`flex m-4 p-4 bg-gray-800 text-white border border-gray-600 rounded-lg w-full focus:ring-2 focus:ring-blue-500 focus:outline-none transition duration-200 ${className}`}
+          className={`px-4 py-2 bg-gray-800 text-white border border-gray-600 rounded-lg w-full focus:ring-2 focus:ring-blue-500 focus:outline-none transition duration-200 ${className}`}
           {...rest}
         />
       </div>
